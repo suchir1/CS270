0dd131e8d88138f62ab25f8c1047b6e0c28bbc65
Suchir <suchir.bhatt@gmail.com>
2018-02-05 00:05:36 -0500

    This might end up working

--- search/searchAgents.py
+++ search/searchAgents.py
@@ -226,6 +226,100 @@ class PositionSearchProblem(search.SearchProblem):
             cost += self.costFn((x,y))
         return cost
 
+class MyPositionSearchProblem(search.SearchProblem):
+    """
+    A search problem defines the state space, start state, goal test, successor
+    function and cost function.  This search problem can be used to find paths
+    to a particular point on the pacman board.
+
+    The state space consists of (x,y) positions in a pacman game.
+
+    Note: this search problem is fully specified; you should NOT change it.
+    """
+
+    def __init__(self, gameState, pos,  costFn = lambda x: 1, goal=(1,1), start=None, warn=True, visualize=False):
+        """
+        Stores the start and goal.
+
+        gameState: A GameState object (pacman.py)
+        costFn: A function from a search state (tuple) to a non-negative number
+        goal: A position in the gameState
+        """
+        self.walls = gameState.getWalls()
+        self.startState = pos
+        if start != None: self.startState = start
+        self.goal = goal
+        self.costFn = costFn
+        self.visualize = visualize
+        if warn and (gameState.getNumFood() != 1 or not gameState.hasFood(*goal)):
+            print 'Warning: this does not look like a regular search maze'
+
+        # For display purposes
+        self._visited, self._visitedlist, self._expanded = {}, [], 0 # DO NOT CHANGE
+
+    def getStartState(self):
+        return self.startState
+
+    def isGoalState(self, state):
+        isGoal = state == self.goal
+
+        # For display purposes only
+        if isGoal and self.visualize:
+            self._visitedlist.append(state)
+            import __main__
+            if '_display' in dir(__main__):
+                if 'drawExpandedCells' in dir(__main__._display): #@UndefinedVariable
+                    __main__._display.drawExpandedCells(self._visitedlist) #@UndefinedVariable
+
+        return isGoal
+
+    def getSuccessors(self, state):
+        """
+        Returns successor states, the actions they require, and a cost of 1.
+
+         As noted in search.py:
+             For a given state, this should return a list of triples,
+         (successor, action, stepCost), where 'successor' is a
+         successor to the current state, 'action' is the action
+         required to get there, and 'stepCost' is the incremental
+         cost of expanding to that successor
+        """
+
+        successors = []
+        for action in [Directions.NORTH, Directions.SOUTH, Directions.EAST, Directions.WEST]:
+            x,y = state
+            dx, dy = Actions.directionToVector(action)
+            nextx, nexty = int(x + dx), int(y + dy)
+            if not self.walls[nextx][nexty]:
+                nextState = (nextx, nexty)
+                cost = self.costFn(nextState)
+                successors.append( ( nextState, action, cost) )
+
+        # Bookkeeping for display purposes
+        self._expanded += 1 # DO NOT CHANGE
+        if state not in self._visited:
+            self._visited[state] = True
+            self._visitedlist.append(state)
+
+        return successors
+
+    def getCostOfActions(self, actions):
+        """
+        Returns the cost of a particular sequence of actions. If those actions
+        include an illegal move, return 999999.
+        """
+        if actions == None: return 999999
+        x,y= self.getStartState()
+        cost = 0
+        for action in actions:
+            # Check figure out the next state and see whether its' legal
+            dx, dy = Actions.directionToVector(action)
+            x, y = int(x + dx), int(y + dy)
+            if self.walls[x][y]: return 999999
+            cost += self.costFn((x,y))
+        return cost
+
+
 class StayEastSearchAgent(SearchAgent):
     """
     An agent for position search with a cost function that penalizes being in
@@ -288,6 +382,7 @@ class CornersProblem(search.SearchProblem):
         # Please add any code here which you would like to use
         # in initializing the problem
         self.cornerDists = {}
+        self.startState = startingGameState
 
     def getStartState(self):
         """
@@ -404,8 +499,48 @@ def cornersHeuristic1(state, problem):
             totalCost[i] +=  distBetweenCorners(corners[i], unvisited)
     return min(totalCost)
 
+def newUniformCostSearch(problem):
+    if(problem.isGoalState(problem.getStartState())):
+        return problem.getStartState()
+    frontier = util.PriorityQueue()
+    visited = {}
+    initialNode = search.Node(problem.getStartState(),[], 0)
+    frontier.push(initialNode, 0)
+    totalPath = 0
+    while 1:
+        if(frontier.isEmpty()):
+            return list()
+        node = frontier.pop()
+        if node.getPos() in visited:
+            continue
+        visited[node.getPos()] = True
+        if problem.isGoalState(node.getPos()):
+            return node.getCost()
+        succ = problem.getSuccessors(node.getPos())
+        for succNode in succ:
+            newPath = node.getPath()[:]
+            newPath.append(succNode[1])
+            tempNode = search.Node(succNode[0],newPath, node.getCost()+succNode[2])
+            tempStack = util.PriorityQueue()
+            insertTemp = True
+            while not frontier.isEmpty():
+                nPrime = frontier.pop()
+                if nPrime.getPos()==tempNode.getPos():
+                    if tempNode.getCost()<nPrime.getCost():
+                        continue
+                    else:
+                        insertTemp = False
+                tempStack.push(nPrime, nPrime.getCost())
+            while not tempStack.isEmpty():
+                bestNode = tempStack.pop()
+                frontier.push(bestNode, bestNode.getCost())
+            if insertTemp:
+                frontier.push(tempNode, tempNode.getCost())
+
 def cornersHeuristic(state, problem):
     cornersBool = state[1]
+    newProb = MyPositionSearchProblem(problem.startState, problem.corners[3], goal=problem.corners[0])
+
 
 
 
